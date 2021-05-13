ec276aa1497d0c444165484008e04e4db436f611
Pineapple6988 <68574157+Pineapple6988@users.noreply.github.com>
2021-05-13 11:16:23 -0400

    Update Gen.rkt

--- Gen.rkt
+++ Gen.rkt
@@ -4,14 +4,6 @@
 
 (require "IOStream.rkt")
 
-;; (Gen inp state step) creates a lazy list
-;;   inp is the overall input (typically but not necessarily a list)
-;;   state is the internal state
-;;   (step inp state cont) returns a list, either directly, or by invoking cont
-;;      (cont newinp newstate newoutput) is a "continuation" that appends the elements of new
-;;       output to the beginning of the list returned by (Gen newinp newstate step)
-
-
 (define (Gen inp state step)
   (define (cont inp state out)
     (append out (Gen inp state step)))
